26e7f728118a6e8d9511cb11da800b12caa266a4
Kulbhushan Mayer <kulbhushan.mayer@gmail.com>
2020-06-11 16:23:13 +0530

    Installing Python 3

--- git-jenkins-demo.sh
+++ git-jenkins-demo.sh
@@ -4,3 +4,10 @@ echo "This is to Show GITHUB & Jenkins Integration Along with POLL SCM Option"
 echo "Showing POLL SCM in Action"
 echo "Demo for triggering Shell or any other Script directly from jenkins Console"
 yum install -y maven
+
+
+echo "=============================="
+yum install -y https://repo.ius.io/ius-release-el7.rpm
+yum update -y
+yum install -y python36u python36u-libs python36u-devel python36u-pip
+python3.6 -V
