a26b7a6dc5a00ccd4bc6d9ac7def4fdc67acc778
Kulbhushan Mayer <kulbhushan.mayer@gmail.com>
2020-06-11 16:33:54 +0530

    Artifact Archive Demo

--- git-jenkins-demo.sh
+++ git-jenkins-demo.sh
@@ -10,4 +10,7 @@ echo "=============================="
 yum install -y https://repo.ius.io/ius-release-el7.rpm
 yum update -y
 yum install -y python36u python36u-libs python36u-devel python36u-pip
+
+date >> demo.txt
+
 exit 0
