85925469fc26ccbbb4ba0c5d8dfd7e9d445d9daa
Kulbhushan Mayer <kulbhushan.mayer@gmail.com>
2020-06-11 16:18:10 +0530

    Demo for triggering Shell or any other Script directly from jenkins Console

--- git-jenkins-demo.sh
+++ git-jenkins-demo.sh
@@ -2,3 +2,4 @@
 
 echo "This is to Show GITHUB & Jenkins Integration Along with POLL SCM Option"
 echo "Showing POLL SCM in Action"
+echo "Demo for triggering Shell or any other Script directly from jenkins Console"
